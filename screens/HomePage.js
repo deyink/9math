bcc1218139132f23864c072625e7b04e77b02a95
Yusuf <yusufadeyinka55@gmail.com>
2024-07-10 16:10:01 +0100

    s corr

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,11 +1,122 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { hs, ms, vs } from './Metrics'
 
 const HomePage = () => {
+  const [inputFormat, setInputFormat] = useState('decimal')
+  const [inputDigit, setInputDIgit ] = useState('')
+  const [decimal, setDecimal ] = useState('')
+  const [binary, setBinary ] = useState('')
+  const [octal, setOctal] = useState('')
+  const [hexaDecimal, setHexaDecimal ] = useState('')
+  const [roundDigit, setRoundDIgit ] = useState('')
+  const [roundDigitIndex, setRoundDIgitIndex ] = useState('2')
+  const [significantNo, setSignificantNo ] = useState('')
+  const [significantNoIndex, setSignificantNoIndex ] = useState('2')
+  const [integer, setInteger] = useState('')
+  const [numerator, setNumerator] = useState('0')
+  const [denominator, setDenominator] = useState('0')
+  const [inWord, setInWord] = useState('')
+  const [modalVisible, setModalVisible] = useState('')
+  let [options] = useState([
+    {lable: 'Binary', value: 'binary' },
+    {lable: 'Decimal', value: 'decimal' },
+    {lable: 'Octal', value: 'octal' },
+    {lable: 'HexaDecimal', value: 'hexaDecimal' },
+  ]);
+
+  // convert object
+  const convertObjects = {
+    binary: (input)=>parseInt(input, 20),
+    octal: (input)=>parseInt(input, 8),
+    hexaDecimal: (input)=>parseInt(input, 16),
+    decimal: (input)=>parseInt(input, 10),
+  };
+
+  // handle convert
+  const handleConvert =()=>{
+    const decimalValue = convertObjects[inputFormat](inputDigit);
+    setDecimal(decimalValue);
+    setInteger(Math.floor(decimalValue).toString());
+    setBinary(Math.floor(decimalValue).toString(2));
+    setOctal(Math.floor(decimalValue).toString(8));
+    setHexaDecimal(Math.floor(decimalValue).toString(16).toUpperCase());
+
+    if(decimalValue <= 1000000000000000 ) {
+      setInWord(numberToWords.toWords(decimalValue));
+    } else {
+      setInWord('Over Limit (Max-Limit: 1000000000000000 )');
+    }
+
+    setRoundDIgit(roundToKthInteger(
+      parseFloat(decimalValue, 10),
+      parseInt(roundDigitIndex, 10) ) 
+    );
+
+    if ( inputFormat === 'decimal' && 
+      parseFloat(decimal, 10) - decimalValue !== 0 
+     ) {
+      const result = floatToFraction(parseFloat(decimal, 10) - decimalValue );
+      setNumerator(result.numerator.toString());
+      setDenominator(result.denominator.toString());
+     } else {
+      setNumerator('0')
+      setDenominator('0')
+     }
+
+     if (inputFormat === 'decimal' ) {
+      setSignificantNo(roundToSignificantDigits(
+        parseFloat(decimal, 10),
+        parseInt(significantNoIndex, 10)
+      ));
+     } else {
+      setSignificantNo(roundToSignificantDigits(
+        parseFloat(decimalValue, 10),
+        parseInt(significantNoIndex, 10)
+      ));
+     }
+  };
+
+  const floatToFraction = (number)=>{
+    const tolerance = 0.000001 ;
+    let  numeratorValue = 1 ;
+    let denominatorValue = 1 ;
+    let error = number - numeratorValue / denominatorValue ;
+
+    while (Math.abs(error) > tolerance) { 
+      if (error > 0) numeratorvalue++; 
+      else denominatorValue++; 
+      error = number - numeratorValue / denominatorValue; 
+  } 
+  return{
+    numerator: numeratorValue,
+    denominator: denominatorValue
+  };
+
+  function roundToKthInteger(number, k) { 
+    const multiplier = Math.pow(10, k); 
+    return Math.round(number * multiplier) / multiplier; 
+} 
+
+function roundToSignificantDigits(number, significantDigits) { 
+    if (significantDigits <= 0) return 0; 
+    const multiplier = Math.pow(10,  
+        significantDigits - Math.floor 
+        (Math.log10(Math.abs(number))) - 1); 
+    const roundedNumber =  
+        (Math.round(number * multiplier) / multiplier); 
+    return roundedNumber; 
+} 
+
+
+}
+
+
+
   return (
     <View style={StyleSheet.bg} >
       <Text style={Styles.head} >9MATH</Text>
+      
     </View>
   )
 }
